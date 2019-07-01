692539d18a39d15aca30c78cc6e0e7b3f51e8621
dongfangxiaoshuo <1563930710@qq.com>
2019-07-01 10:21:01 +0800

    Initial commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <van-button type="primary">按钮</van-button>
+   123123
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data () {
+    return {
+     
+    }
+  },
+
+
+
+  methods: {
+   
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/me/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div @click="clickHandle">
+
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data () {
+    return {
+     
+    }
+  },
+
+  components: {
+  
+  },
+
+  methods: {
+   
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
